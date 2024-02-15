b8bc79811f940b16f34a58bfecab05670c84729f
Maden51 <madenon123@gmail.com>
2024-02-15 11:43:41 +0300

    Added form, fixed regular expression

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const input = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const resultBox = document.getElementById('result');
+const form = document.getElementById('palindrome-form');
 
-checkButton.addEventListener('click', () => {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
   if (input.value === '') {
     alert('Please input a value');
   } else {
     resultBox.innerHTML = '';
     if (checkPalindrome(input.value) === true) {
       resultBox.innerHTML = `<p><strong>${input.value}</strong> is a palindrome</p>`;
+      input.value = '';
     } else {
       resultBox.innerHTML = `<p><strong>${input.value}</strong> is not a palindrome</p>`;
+      input.value = '';
     }
     resultBox.style.display = 'block';
   }
@@ -19,12 +23,10 @@ checkButton.addEventListener('click', () => {
 function checkPalindrome(str) {
   let cleanStr = checkStrInput(str);
   let strReverse = cleanStr.split('').reverse().join('');
-  console.log(cleanStr);
-  console.log(strReverse);
   return strReverse.toLowerCase() === cleanStr.toLowerCase() ? true : false;
 };
 
 function checkStrInput(str) {
-  const regex = /[\W\s_]/g;
+  const regex = /[,.\-\\\/:+_?!\s]/g;
   return str.replace(regex,'');
 }
